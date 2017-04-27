838199f6ca2fec31cbef22901b423c165d3066c6
Maxime Dor <max@kamax.io>
2017-04-27 18:01:39 +0200

    Add 3PID medium type enum

---
+++ src/main/java/io/kamax/matrix/ThreePidMedium.java
@@ -0,0 +1,46 @@
+/*
+ * matrix-java-sdk - Matrix Client SDK for Java
+ * Copyright (C) 2017 Maxime Dor
+ *
+ * https://max.kamax.io/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.kamax.matrix;
+
+public enum ThreePidMedium {
+
+    Email("email"),
+    PhoneNumber("msisdn");
+
+    private String id;
+
+    ThreePidMedium(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String toString() {
+        return getId();
+    }
+
+    public boolean is(String s) {
+        return getId().contentEquals(s);
+    }
+
+}
