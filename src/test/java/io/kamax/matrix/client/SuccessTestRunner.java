04912b8d1d3c77d64584a939a43a8ad091ea7d8f
Arne Augenstein <devel@arne-augenstein.de>
2017-10-02 22:19:22 +0200

    Fix error in runner

--- src/test/java/io/kamax/matrix/client/SuccessTestRunner.java
+++ src/test/java/io/kamax/matrix/client/SuccessTestRunner.java
@@ -1,3 +1,23 @@
+/*
+ * matrix-java-sdk - Matrix Client SDK for Java
+ * Copyright (C) 2017 Arne Augenstein
+ *
+ * https://max.kamax.io/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.kamax.matrix.client;
 
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
@@ -45,10 +65,12 @@ public class SuccessTestRunner<R, P> {
     private ResponseDefinitionBuilder createResponse() {
         ResponseDefinitionBuilder response = aResponse().withStatus(responseBuilder.getStatus());
         Optional<String> body = responseBuilder.getBody();
+        Optional<String> bodyFile = responseBuilder.getBodyFile();
+
         if (body.isPresent()) {
             response.withBody(body.get());
-        } else {
-            response.withBodyFile(responseBuilder.getBodyFile().get());
+        } else if (bodyFile.isPresent()) {
+            response.withBodyFile(bodyFile.get());
         }
 
         Optional<String> contentType = responseBuilder.getContentType();
