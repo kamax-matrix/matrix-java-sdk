b52c63035b86bffb7a8cd3eba06cfba87f432ef1
Maxime Dor <max@kamax.io>
2017-12-18 10:41:38 +0100

    Correctly handle Matrix IDs with port in them

--- src/test/java/io/kamax/matrix/MatrixIDTest.java
+++ src/test/java/io/kamax/matrix/MatrixIDTest.java
@@ -29,6 +29,8 @@ public class MatrixIDTest {
     private static String validMxId1 = "@john.doe:example.org";
     private static String validMxId2 = "@john.doe:example.com";
     private static String validMxId3 = "@JoHn.dOe:ExamPLE.ORG";
+    private static String validMxid4 = "@john:example.org:8449";
+    private static String validMxid5 = "@john:::1:8449";
 
     private static String invalidMxId1 = "john.doe:example.org";
     private static String invalidMxId2 = "@john.doeexample.org";
@@ -40,9 +42,13 @@ public class MatrixIDTest {
     public void validMatrixIDs() {
         _MatrixID mxId1 = new MatrixID(validMxId1);
         _MatrixID mxId3 = new MatrixID(validMxId3);
+        _MatrixID mxId4 = new MatrixID(validMxid4);
+        _MatrixID mxId5 = new MatrixID(validMxid5);
         assertTrue(validMxId1.contentEquals(mxId1.getId()));
         assertTrue("john.doe".contentEquals(mxId1.getLocalPart()));
         assertTrue("example.org".contentEquals(mxId1.getDomain()));
+        assertTrue("example.org:8449".contentEquals(mxId4.getDomain()));
+        assertTrue("::1:8449".contentEquals(mxId5.getDomain()));
     }
 
     @Test
